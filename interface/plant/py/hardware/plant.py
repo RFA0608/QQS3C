1ac54e500f2826b4b1d66e960e48fc5d537813ad
Mingyu Jeong <159212138+RFA0608@users.noreply.github.com>
2026-02-13 15:54:31 +0900

    Simplify control loop and clean up code
    Refactor control logic and remove unused variables.

--- interface/plant/py/hardware/plant.py
+++ interface/plant/py/hardware/plant.py
@@ -5,7 +5,7 @@ from pal.products.qube import QubeServo2, QubeServo3
 from pal.utilities.math import SignalGenerator, ddt_filter
 from pal.utilities.scope import Scope
 
-sys.path.append(r"./communication/py")
+sys.path.append(r"./py")
 import tcp_protocol_server as tcs
 
 # init tcp host and port
@@ -17,7 +17,6 @@ from threading import Thread
 import signal
 import time
 import math
-import control as ct
 import numpy as np
 
 # Thread hanlder initialization
@@ -72,60 +71,9 @@ def control_loop():
     # class initialization
     QubeClass = QubeServo3
 
-    # for state estimation
-    state_theta_dot = np.array([0,0], dtype=np.float64)
-    state_alpha_dot = np.array([0,0], dtype=np.float64)
-
     # swing-up standing gate
     stand_run = False
-    change_flag = False
-    set_time = 0
-    switching_time = 1
-
-    # gain of full-state(use initial part of swing-up)
-    A = np.array([[0, 0, 1, 0],
-                  [0, 0, 0, 1],
-                  [0, 149.275096865093, -0.0130994260613721, 0],
-                  [0, 261.609107366662, -0.0129471071536817, 0]], dtype=float)
-    
-    B = np.array([[0],
-                  [0],
-                  [55.6948386963098],
-                  [55.0472242928643]], dtype=float)
-    
-    C = np.array([[1, 0, 0, 0],
-                  [0, 1, 0, 0],
-                  [0, 0, 1, 0],
-                  [0, 0, 0, 1]], dtype=float)
-    
-    D = np.array([[0],
-                  [0],
-                  [0],
-                  [0]], dtype=float)
-    
-    sys_c = ct.ss(A, B, C, D)
-    sys_d = sys_c.sample((1 / frequency), method='zoh')
-
-    Q_k = np.array([[5, 0, 0, 0],
-                    [0, 1, 0, 0],
-                    [0, 0, 1, 0],
-                    [0, 0, 0, 1]], dtype=float)
-    R_k = np.array([[1]], dtype=float)
-    K, Sk, Ek = ct.dlqr(sys_d.A, sys_d.B, Q_k, R_k)
-
-    # Physical Parameters
-    mp = 0.024       
-    Lp = 0.129       
-    l = Lp / 2       
-    g = 9.81         
-    Jp = mp * (Lp**2) / 3  
-
-    # Control Parameters
-    E_ref = 0.0      
-    mu = 150.0  
-
-    # switching target angle
-    angle = 10
+    er = 0.02
 
     # describe #
     # ------------------------------------------------ #
@@ -148,46 +96,13 @@ def control_loop():
                     theta = myQube.motorPosition * -1
                     alpha_f =  myQube.pendulumPosition
                     alpha = np.mod(alpha_f, 2*np.pi) - np.pi
-                    alpha_deg = abs(math.degrees(alpha))
+                    alpha_deg = alpha * 180 / np.pi
 
-                    # Calculate angular velocities with filter of 50 and 100 rad
-                    theta_dot, state_theta_dot = ddt_filter(theta, state_theta_dot, 50, 1/frequency)
-                    alpha_dot, state_alpha_dot = ddt_filter(alpha, state_alpha_dot, 100, 1/frequency)
-                    states = np.array([theta, alpha, theta_dot, alpha_dot])
-
-                    # Energy Calculation
-                    E_pot = mp * g * l * (math.cos(alpha) - 1)
-                    E_kin = 0.5 * Jp * (alpha_dot**2)
-                    E_total = E_pot + E_kin
-                    E_err = E_ref - E_total
-
-                    # calc control rate
-                    term = np.sign(alpha_dot * math.cos(alpha))
-                    if abs(alpha_dot) < 0.05 and abs(math.cos(alpha)) < 0.1:
-                        term = 0
-                    u_raw = -1 * mu * E_err * term
-
-                    if(alpha_deg > angle and (not change_flag)):
-                        if(-theta > 0):
-                            if(u_raw > 0):
-                                voltage = 0.0
-                            else:
-                                voltage = u_raw
-                        else:
-                            if(u_raw < 0):
-                                voltage = 0.0
-                            else:
-                                voltage = u_raw
-                    else:
-                        voltage = 1*np.dot(K, states)
-        
-                        if(not change_flag):
-                            change_flag = True
-                            set_time = timeStamp
-                            stand_run = True
+                    if abs(alpha) < er and abs(theta) < er:
+                        stand_run = True
                     
+                    voltage = 0
                     # write commands
-                    voltage = np.clip(voltage, -8, 8)
                     myQube.write_voltage(voltage)
 
                     print(f"control start: {stand_run}")
@@ -203,11 +118,6 @@ def control_loop():
                     alpha_f =  myQube.pendulumPosition
                     alpha = np.mod(alpha_f, 2*np.pi) - np.pi
                     alpha_deg = alpha * 180 / np.pi
-
-                    # Calculate angular velocities with filter of 50 and 100 rad
-                    theta_dot, state_theta_dot = ddt_filter(theta, state_theta_dot, 50, 1/frequency)
-                    alpha_dot, state_alpha_dot = ddt_filter(alpha, state_alpha_dot, 100, 1/frequency)
-                    states = np.array([theta, alpha, theta_dot, alpha_dot])
     
                     # send plant output
                     tcsp.send(-theta)
@@ -216,18 +126,12 @@ def control_loop():
                     # get control input
                     _, u = tcsp.recv()
     
-                    # swing up trasient responce cushioning
-                    if(timeStamp - set_time < switching_time):
-                        voltage = 1*np.dot(K, states)
-                        print("control object: full-state on plant")
+                    # running range set
+                    if abs(alpha_deg) < 15:
+                        voltage = u
                     else:
-                        # running range set
-                        if abs(alpha_deg) < 25:
-                            voltage = u
-                        else:
-                            voltage = 0
-                        print("control object: outside controller")
-                    
+                        voltage = 0
+    
                     # write commands
                     myQube.write_voltage(voltage)
 
@@ -244,10 +148,10 @@ def control_loop():
             tcsp.send("end")
 
 def main():
-    thread_cl = Thread(target=control_loop)
-    thread_cl.start()
+    thread = Thread(target=control_loop)
+    thread.start()
 
-    while thread_cl.is_alive() and (not KILL_THREAD):
+    while thread.is_alive() and (not KILL_THREAD):
 
         # This must be called regularly or the scope windows will freeze
         # Must be called in the main thread.
